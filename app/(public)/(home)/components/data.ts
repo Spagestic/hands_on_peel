291c1a0fafd92a59f099f5d6aa5b09d96792417d
spagestic <steelmax666@gmail.com>
2026-03-26 11:09:47 +0800

    feat: add new craftsmen to the craftsmen list for enhanced representation

--- app/(public)/(home)/components/data.ts
+++ app/(public)/(home)/components/data.ts
@@ -110,6 +110,36 @@ export const craftsmen = [
     image: "",
     href: "/craftsmen/wong-siu",
   },
+  {
+    name: "Lam Chun",
+    craft: "Paper crafting",
+    image: "",
+    href: "/craftsmen/lam-chun",
+  },
+  {
+    name: "Cheung Yan",
+    craft: "Wood carving",
+    image: "",
+    href: "/craftsmen/cheung-yan",
+  },
+  {
+    name: "Lee Ming",
+    craft: "Ceramics",
+    image: "",
+    href: "/craftsmen/lee-ming",
+  },
+  {
+    name: "Kwok Yin",
+    craft: "Galvanised iron",
+    image: "",
+    href: "/craftsmen/kwok-yin",
+  },
+  {
+    name: "Tam Lai",
+    craft: "Seal carving",
+    image: "",
+    href: "/craftsmen/tam-lai",
+  },
 ];
 
 export const upcomingEvents = [
