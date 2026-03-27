7d8fd43ae18f1fc8597c4f4083f3fbcf8b8050c0
spagestic <steelmax666@gmail.com>
2026-03-27 15:31:51 +0800

    moved data

--- app/(public)/(home)/components/data/index.ts
+++ app/(public)/(home)/components/data/index.ts
@@ -1,6 +1,5 @@
-export { currentExhibition } from "./current-exhibition";
+export { currentExhibition } from "@/data/current-exhibition";
 export { featuredArtisan } from "./featured-artisan";
 export { everydayCrafts } from "./everyday-crafts";
-export { exhibitionArchive } from "./exhibition-archive";
+export { exhibitionArchive } from "@/data/exhibition-archive";
 export { craftsmen } from "./craftsmen";
-export { pastEvents, workshopShowcase } from "./events";
